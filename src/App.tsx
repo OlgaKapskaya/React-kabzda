5f5f01fd4c875affc3989f98a7523974ba996990
OlgaKapskaya <olikbuko@gmail.com>
2022-09-05 09:10:15 +0300

    lesson 9_05.09.2022

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,35 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
      Hello, samurai! Let's go!
+        <Header />
+        <Technologies />
     </div>
   );
+}
+
+const Header = () => {
+    return (
+        <div>
+            <a href = '#s'>Home</a>
+            <a href = '#s'>News Feed</a>
+            <a href = '#s'>Messages</a>
+        </div>
+    )
+}
+
+const Technologies = () => {
+    return (
+        <div>
+            <ul>
+                <li>css</li>
+                <li>html</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>
+    )
 }
 
 export default App;
