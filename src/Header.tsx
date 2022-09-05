099ae126fc0fef664f812a5f4a9f38bb4dbe189d
OlgaKapskaya <olikbuko@gmail.com>
2022-09-05 18:55:47 +0300

    components 05.09.2022

--- src/Header.tsx
+++ src/Header.tsx
@@ -3,10 +3,10 @@ import React from "react";
 const Header = () => {
     return (
         <div>
-            <a href = '#s'>Home</a>
-            <a href = '#s'>News</a>
-            <a href = '#s'>Feed</a>
-            <a href = '#s'>Messages</a>
+            <a href = '#s'>Home </a>
+            <a href = '#s'>News </a>
+            <a href = '#s'>Feed </a>
+            <a href = '#s'>Messages </a>
         </div>
     )
 }
